cc66f5ff35963c384b1aab3eabe3b8f2d2524577
Bert JW Regeer <bert@redrangerz.com>
2019-10-16 11:51:08 -0700

    Remove old acceptor code and include header file

--- src/websockets/wrapper.cpp
+++ src/websockets/wrapper.cpp
@@ -4,78 +4,12 @@
 
 #include <websocketswrapper.h>
 #include <wsinternal/bridge.h>
+#include <wsinternal/acceptor.h>
+
+namespace net = boost::asio;
+using wsinternal::wsstream;
 
 
-namespace internal {
-    class acceptor {
-        public:
-            acceptor(
-                asio::io_context& io_context,
-                const asio::ip::address_v4& local_host,
-                unsigned short local_port,
-                std::function<void(asio::ip::tcp::socket)> sockethandler
-            ) :
-                io_context_(io_context),
-                socket_(io_context_),
-                localhost_address(local_host),
-                acceptor_(
-                    io_context_,
-                    asio::ip::tcp::endpoint(
-                        localhost_address,
-                        local_port
-                    )
-                ),
-                sockethandler_(sockethandler)
-            {}
-
-            asio::ip::tcp::endpoint local_endpoint() {
-                return acceptor_.local_endpoint();
-            }
-
-            bool accept_connections()
-            {
-                try
-                {
-                    acceptor_.async_accept(socket_,
-                        std::bind(&acceptor::handle_accept,
-                            this,
-                            std::placeholders::_1));
-                }
-                catch (std::exception& e)
-                {
-                    std::cerr << "Unable to start accepting connections: " << e.what() << std::endl;
-                    return false;
-                }
-
-                return true;
-            }
-
-        private:
-
-            void handle_accept(const asio::error_code& error)
-            {
-                if (!error)
-                {
-                    sockethandler_(std::move(socket_));
-
-                    if (!accept_connections())
-                    {
-                        std::cerr << "Accepted connection, and now can't accept more!" << std::endl;
-                    }
-                }
-                else
-                {
-                    std::cerr << "Error: " << error.message() << std::endl;
-                }
-            }
-
-            asio::io_context& io_context_;
-            asio::ip::tcp::socket socket_;
-            asio::ip::address localhost_address;
-            asio::ip::tcp::acceptor acceptor_;
-            std::function<void(asio::ip::tcp::socket)> sockethandler_;
-    };
-}
 
 WebsocketsWrapper::WebsocketsWrapper(std::string c2_uri) :
     pimpl_(std::make_unique<WebsocketsWrapper::impl>(c2_uri))
@@ -100,12 +34,12 @@ class WebsocketsWrapper::impl {
             ),
             acceptor_(
                 io_context_,
-                asio::ip::address_v4::loopback(),
+                net::ip::address_v4::loopback(),
                 0,
                 [&] (asio::ip::tcp::socket socket) {
                     auto bridge = internal::bridge<wswrap::client::connection_ptr>::create(std::move(socket), std::move(websocket_connection));
                 }
-            ),
+            )
         {
             acceptor_.accept_connections();
 
@@ -125,10 +59,11 @@ class WebsocketsWrapper::impl {
 
     private:
         std::string uri_;
-        asio::io_context io_context_;
-        std::shared_ptr<asio::io_context::work> aio_work_;
+        net::io_context io_context_;
+        net::ssl::context ssl_context_;
+        std::shared_ptr<net::io_context::work> aio_work_;
         std::thread io_runner_;
-        internal::acceptor acceptor_;
+        wsinternal::acceptor acceptor_;
 
     public:
         std::string local_ip_;
