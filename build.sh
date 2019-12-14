5a36ef65b8288269a2a4cdeb0e9e12f7b9d5fe87
illera88 <agarciaillera@gmail.com>
2019-12-14 09:15:57 +0000

    add libdbus instalation to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
 echo "Installing some dependencies we need to build"
+
+echo "Installing dbus static"
+wget https://dbus.freedesktop.org/releases/dbus/dbus-1.12.16.tar.gz
+tar -xzvf dbus-1.12.16.tar.gz && cd dbus-1.12.16
+./configure --prefix=/usr                        \
+            --sysconfdir=/etc                    \
+            --localstatedir=/var                 \
+            --enable-user-session                \
+            --disable-doxygen-docs               \
+            --disable-xml-docs                   \
+            --with-systemduserunitdir=no         \
+            --with-systemdsystemunitdir=no       \
+            --docdir=/usr/share/doc/dbus-1.12.16 \
+            --with-console-auth-dir=/run/console \
+            --with-system-pid-file=/run/dbus/pid \
+            --with-system-socket=/run/dbus/system_bus_socket && make && make install && cd ..
+
+
 apk add --no-cache \
     build-base \
     cmake \
@@ -18,7 +36,8 @@ apk add --no-cache \
     boost-system \
     openssl \
     openssl-libs-static \
-    openssl-dev
+    openssl-dev \ 
+    networkmanager-dev
 
 # Building CidSSH
 rm -rf build
