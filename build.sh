80a0f1a27cf1d3ee73fb3ba2420228de92598855
Alberto Garcia Illera <agarciaillera@gmail.com>
2022-10-27 17:48:53 +1100

    small fixes and improve linux install

--- build.sh
+++ build.sh
@@ -8,61 +8,60 @@ echo "*** Current value of \$PASSWORD_AUTH is '$PASSWORD_AUTH' and \$C2_IP is '$
 echo "Installing some dependencies we need to build"
 apk update
 
-apk add --no-cache \
-    build-base \
-    cmake \
-    autoconf \
-    libtool \
-    pkgconf \
-    git \
-    mercurial \
-    file \
-    linux-headers \
-    wget \
-    libc-dev \
-    boost-dev \
-    boost-static \
-    boost-system \
-    openssl \
-    openssl-dev \
-    networkmanager-dev \
-    glib-dev \
-    expat-dev \
-    openssl-libs-static \
-    libc6-compat \
-    cmake \
-    ninja \
-    zip \
-    unzip \
-    curl
-
 echo "Installing vcpkg"
-(	cd /tmp
+(	
+    # vcpkg dependencies
+    apk add --no-cache build-base cmake ninja zip unzip curl git
+    # OpenSSL dependencies
+    apk add --no-cache linux-headers perl pkgconf
+
+    cd /tmp
 	git clone https://github.com/Microsoft/vcpkg.git
 	cd vcpkg
 	echo "set(VCPKG_BUILD_TYPE release)" >> /tmp/vcpkg/triplets/x64-linux.cmake
-	./bootstrap-vcpkg.sh --useSystemBinaries -disableMetrics
+	./bootstrap-vcpkg.sh -disableMetrics
 	VCPKG_FORCE_SYSTEM_BINARIES=1 /tmp/vcpkg/vcpkg install libssh[core,openssl] --triplet x64-linux
 )
-	
-echo "Installing dbus static"
-(
-	cd /tmp
-	wget https://dbus.freedesktop.org/releases/dbus/dbus-1.12.16.tar.gz
-	tar -xzvf dbus-1.12.16.tar.gz && cd dbus-1.12.16
-	./configure --prefix=/usr                        \
-				--sysconfdir=/etc                    \
-				--localstatedir=/var                 \
-				--enable-user-session                \
-				--disable-doxygen-docs               \
-				--disable-xml-docs                   \
-				--with-systemduserunitdir=no         \
-				--with-systemdsystemunitdir=no       \
-				--docdir=/usr/share/doc/dbus-1.12.16 \
-				--with-console-auth-dir=/run/console \
-				--with-system-pid-file=/run/dbus/pid \
-				--with-system-socket=/run/dbus/system_bus_socket && make -j$(nproc) && make install
-)
+
+# echo "Installing libproxy dependencies"
+# (
+#     apk add --no-cache \
+#         autoconf \
+#         libtool \
+#         mercurial \
+#         file \
+#         wget \
+#         libc-dev \
+#         boost-dev \
+#         boost-static \
+#         boost-system \
+#         openssl \
+#         openssl-dev \
+#         networkmanager-dev \
+#         glib-dev \
+#         expat-dev \
+#         openssl-libs-static \
+#         libc6-compat
+# )
+
+# echo "Installing dbus static (libproxy dependency)"
+# (
+# 	cd /tmp
+# 	wget https://dbus.freedesktop.org/releases/dbus/dbus-1.12.16.tar.gz
+# 	tar -xzvf dbus-1.12.16.tar.gz && cd dbus-1.12.16
+# 	./configure --prefix=/usr                        \
+# 				--sysconfdir=/etc                    \
+# 				--localstatedir=/var                 \
+# 				--enable-user-session                \
+# 				--disable-doxygen-docs               \
+# 				--disable-xml-docs                   \
+# 				--with-systemduserunitdir=no         \
+# 				--with-systemdsystemunitdir=no       \
+# 				--docdir=/usr/share/doc/dbus-1.12.16 \
+# 				--with-console-auth-dir=/run/console \
+# 				--with-system-pid-file=/run/dbus/pid \
+# 				--with-system-socket=/run/dbus/system_bus_socket && make -j$(nproc) && make install
+# )
 
 
 # Building CidSSH
